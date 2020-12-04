b4750ba09c8af76ce37a55776ea9c595018a3803
Aarnavagarwal69 <61588119+Aarnavagarwal69@users.noreply.github.com>
2020-12-04 16:17:58 +0400

    Add files via upload

---
+++ screens/recieverdetails.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, Modal, KeyboardAvoidingView, ScrollView, Alert } from 'react-native';
+
+
+export default class Recieverdetailsscreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    reciever details screen
+                </Text>
+            </View>
+        )
+    }
+}
+    
